e2534f29c925896a97b6f9c9acc8f6e04d5cfa86
Jesse370 <troiaj@sunypoly.edu>
2021-03-01 21:38:33 -0500

    More move
    Now with jumping

--- Aether World/Assets/Scripts/PlayerMovement.cs
+++ Aether World/Assets/Scripts/PlayerMovement.cs
@@ -5,13 +5,25 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float movementSpeed;
+    public float JumpForce = 10f;
     public Rigidbody2D rb;
 
+    public Transform feet;
+    public LayerMask groundLayers;
+
     float movex;
+
+   
 
     private void Update()
     {
         movex = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            Jump();
+
+        }
     }
 
     private void FixedUpdate()
@@ -19,5 +31,25 @@ public class PlayerMovement : MonoBehaviour
         Vector2 movement = new Vector2(movex * movementSpeed, rb.velocity.y);
 
         rb.velocity = movement;
+    }
+
+
+    void Jump()
+    {
+        Vector2 movement = new Vector2(rb.velocity.x, JumpForce);
+
+        rb.velocity = movement;
+
+    }
+
+    public bool IsGrounded()
+    {
+        Collider2D groundCheck = Physics2D.OverlapCircle(feet.position, 0.5f, groundLayers);
+
+        if (groundCheck != null)
+        {
+            return true;
+        }
+        return false;
     }
 }
