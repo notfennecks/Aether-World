1404fbf541ed7bb8cca03ad7426e7ceaebf51e4c
Dylan Santiago <50032722+WayOfOld@users.noreply.github.com>
2021-03-03 03:25:06 -0500

    Stance Switching

--- Aether World/Assets/Scripts/PlayerMovement.cs
+++ Aether World/Assets/Scripts/PlayerMovement.cs
@@ -5,15 +5,30 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float movementSpeed;
-    public float JumpForce = 10f;
+    public float jumpForce;
     public Rigidbody2D rb;
 
     public Transform feet;
     public LayerMask groundLayers;
 
+    public string currentStance;
+    public string stance;
+    public SpriteRenderer spriteRender;
+
+    [Header("Stance 1 Variables")]
+    public float stanceFastMovementSpeed;
+    public float stanceFastJumpForce;
+
+    [Header("Stance 2 Variables")]
+    public float stanceStrongMovementSpeed;
+    public float stanceStrongJumpForce;
+
     float movex;
 
-   
+    private void Awake()
+    {
+        SwitchStance("FAST");
+    }
 
     private void Update()
     {
@@ -24,6 +39,19 @@ public class PlayerMovement : MonoBehaviour
             Jump();
 
         }
+
+        //All the stance switching code below is pretty straighforward. I just commented out debugs cause they get annoying in console.
+        if (Input.GetButtonDown("Stance 1"))
+        {
+            SwitchStance("FAST");
+            //Debug.Log("Switched to " + currentStance + " stance!");
+        }
+        if (Input.GetButtonDown("Stance 2"))
+        {
+            SwitchStance("STRONG");
+            //Debug.Log("Switched to " + currentStance + " stance!");
+        }
+
     }
 
     private void FixedUpdate()
@@ -36,7 +64,7 @@ public class PlayerMovement : MonoBehaviour
 
     void Jump()
     {
-        Vector2 movement = new Vector2(rb.velocity.x, JumpForce);
+        Vector2 movement = new Vector2(rb.velocity.x, jumpForce);
 
         rb.velocity = movement;
 
@@ -44,6 +72,7 @@ public class PlayerMovement : MonoBehaviour
 
     public bool IsGrounded()
     {
+        //We dont want player clinging to walls in every stance we create, so we are gonna need to find a different way to do this.
         Collider2D groundCheck = Physics2D.OverlapCircle(feet.position, 0.5f, groundLayers);
 
         if (groundCheck != null)
@@ -51,5 +80,35 @@ public class PlayerMovement : MonoBehaviour
             return true;
         }
         return false;
+    }
+
+    //This function just matches current value of stance and executes blocks of codes based on its value. (Default does nothing ATM)
+    public void SwitchStance(string stance)
+    {
+        if (currentStance == stance)
+        {
+            Debug.Log("Already in that stance silly goose");
+            return;
+        }
+        else
+        {
+            switch (stance)
+            {
+                case "FAST":
+                    currentStance = stance;
+                    movementSpeed = stanceFastMovementSpeed;
+                    jumpForce = stanceFastJumpForce;
+                    spriteRender.color = Color.blue;
+                    break;
+                case "STRONG":
+                    currentStance = stance;
+                    movementSpeed = stanceStrongMovementSpeed;
+                    jumpForce = stanceStrongJumpForce;
+                    spriteRender.color = Color.red;
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
