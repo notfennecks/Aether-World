d50b79c982ca5ca43bedfd21d128b075a6bfdc13
Dylan Santiago <50032722+WayOfOld@users.noreply.github.com>
2021-03-23 21:20:16 -0400

    Two UI Sounds

--- Aether World/Assets/Scripts/LevelSelection.cs
+++ Aether World/Assets/Scripts/LevelSelection.cs
@@ -5,21 +5,36 @@ using UnityEngine.SceneManagement;
 
 public class LevelSelection : MonoBehaviour
 {
-
+    [Header("Animation Variables")]
     public Animator overworldAnimator;
     public Animator terraAnimator;
     public Animator scoriaAnimator;
     public Animator eldorisAnimator;
     public Animator zephyrAnimator;
 
+    [Header("Audio Source Variables")]
+    public AudioSource overworldSelect;
+    public AudioSource overworldHover;
+    public AudioSource terraSelect;
+    public AudioSource terraHover;
+    public AudioSource scoriaSelect;
+    public AudioSource scoriaHover;
+    public AudioSource eldorisSelect;
+    public AudioSource eldorisHover;
+    public AudioSource zephyrSelect;
+    public AudioSource zephyrHover;
+
     public void SelectOverworld()
     {
-        Debug.Log("Selected Overworld!");
+        //Debug.Log("Selected Overworld!");
+        if (overworldSelect.isPlaying != true)
+            overworldSelect.Play();
     }
 
     public void HoverOverworld()
     {
         overworldAnimator.Play("OverworldGrow");
+        overworldHover.Play();
     }
 
     public void ExitHoverOverworld()
@@ -29,12 +44,14 @@ public class LevelSelection : MonoBehaviour
 
     public void SelectTerra()
     {
-        Debug.Log("Selected Terra!");
+        //Debug.Log("Selected Terra!");
+        terraSelect.Play();
     }
 
     public void HoverTerra()
     {
         terraAnimator.Play("TerraGrow");
+        terraHover.Play();
     }
 
     public void ExitHoverTerra()
@@ -44,12 +61,14 @@ public class LevelSelection : MonoBehaviour
 
     public void SelectScoria()
     {
-        Debug.Log("Selected Scoria!");
+        //Debug.Log("Selected Scoria!");
+        scoriaSelect.Play();
     }
 
     public void HoverScoria()
     {
         scoriaAnimator.Play("ScoriaGrow");
+        scoriaHover.Play();
     }
 
     public void ExitHoverScoria()
@@ -59,12 +78,14 @@ public class LevelSelection : MonoBehaviour
 
     public void SelectEldoris()
     {
-        Debug.Log("Selected Eldoris!");
+        //Debug.Log("Selected Eldoris!");
+        eldorisSelect.Play();
     }
 
     public void HoverEldoris()
     {
         eldorisAnimator.Play("EldorisGrow");
+        eldorisHover.Play();
     }
 
     public void ExitHoverEldoris()
@@ -74,16 +95,19 @@ public class LevelSelection : MonoBehaviour
 
     public void SelectZephyr()
     {
-        Debug.Log("Selected Zephyr!");
+        //Debug.Log("Selected Zephyr!");
+        zephyrSelect.Play();
     }
 
     public void HoverZephyr()
     {
         zephyrAnimator.Play("ZephyrGrow");
+        zephyrHover.Play();
     }
 
     public void ExitHoverZephyr()
     {
         zephyrAnimator.Play("ZephyrShrink");
     }
+
 }
