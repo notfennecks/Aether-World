2a20f10acb3d35bcc54441a51c1bac96e9bc1a47
Jesse370 <troiaj@sunypoly.edu>
2021-04-07 17:12:52 -0400

    Added goal point and air jump
    Right click while in air essence I dare you.

--- Aether World/Assets/Scripts/LevelManager.cs
+++ Aether World/Assets/Scripts/LevelManager.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
     public static LevelManager instance;
     public Transform RespwanPoint;
     public GameObject PlayerPrefab;
+    public GameObject GoalPoint;
 
     private void Awake()
     {
@@ -16,5 +18,10 @@ public class LevelManager : MonoBehaviour
     public void Respawn()
     {
         Instantiate(PlayerPrefab, RespwanPoint.position, Quaternion.identity);
+    }
+
+    public void Victory()
+    {
+        SceneManager.LoadScene("LevelSelection");//There need to be something here to show the level was beaten, not just exited.
     }
 }
---
+++ Aether World/Assets/Scripts/Player/Abilities/AirJump.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AirJump : MonoBehaviour
+{
+    private GameObject player;
+    private EssenceManager em;
+    private PlayerMovement pm;
+    private bool CanUse;
+    public float CoolDown;
+    float CoolDownTime;
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        em = player.GetComponent<EssenceManager>();
+        pm = player.GetComponent<PlayerMovement>();
+        CanUse = true;
+    }
+
+    void Update()
+    {
+        if (em.currentEssence == "AIR" && Input.GetMouseButtonDown(1))
+        {
+            if (CanUse == true)
+            {
+                Vector2 movement = new Vector2(pm.rb.velocity.x, 25f);
+                pm.rb.velocity = movement;
+                CoolDownTime = Time.time + CoolDown;
+                CanUse = false;
+                Debug.Log("Boing");
+            }
+        }
+        if (CoolDownTime < Time.time)
+        {
+            CanUse = true;
+        }
+    }
+    
+}
