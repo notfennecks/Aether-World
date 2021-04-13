667d800cc4029d26676b5d9f56c879e8b55f5376
Dylan Santiago <50032722+WayOfOld@users.noreply.github.com>
2021-04-12 23:09:06 -0400

    Deep One Enemy Finished

---
+++ Aether World/Assets/Enemies/DeepOne/DeepOneAI.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeepOneAI : MonoBehaviour
+{
+    public float speed;
+    public float detectDistance;
+    public float spitDistance;
+    private Transform player;
+    private Animator anim;
+    public GameObject spit;
+    public GameObject spitParent;
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        Physics2D.IgnoreCollision(player.GetComponent<BoxCollider2D>(), GetComponent<BoxCollider2D>());
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+
+        if (this.transform.position.x > player.position.x)
+        {
+            this.transform.localScale = new Vector3(1, 1, 0);
+        }
+        else
+        {
+            this.transform.localScale = new Vector3(-1, 1, 0);
+        }
+
+        float distFromPlayer = Vector2.Distance(player.position, transform.position);
+        if (distFromPlayer < detectDistance && distFromPlayer > spitDistance)
+        {
+            transform.position = Vector2.MoveTowards(this.transform.position, new Vector2(player.position.x, this.transform.position.y), speed * Time.deltaTime);
+        }
+        if(distFromPlayer < spitDistance)
+        {
+            anim.SetBool("canAttack", true);
+        }
+        else
+        {
+            anim.SetBool("canAttack", false);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, detectDistance);
+        Gizmos.DrawWireSphere(transform.position, spitDistance);
+    }
+
+    private void Spit()
+    {
+        Instantiate(spit, spitParent.transform.position, Quaternion.identity);
+    }
+}
--- Aether World/Assets/Scripts/SocialSpace.cs
+++ Aether World/Assets/Scripts/SocialSpace.cs
@@ -6,43 +6,8 @@ using UnityEngine.SceneManagement;
 public class SocialSpace : MonoBehaviour
 {
 
-    public GameObject player;  //Stores player object refference
-    Vector2 playerPosition;  //Used to store player position
-
-    bool canUseLevelSelectionBoard = false;  //If player can interact with level selection board
-
-    public SpriteRenderer selectionBoard;
-
-
     private void Update()
     {
-        playerPosition = player.transform.position;  //Updates player position
-        if (canUseLevelSelectionBoard == true && Input.GetKeyDown(KeyCode.E) == true)
-        {
-            SceneManager.LoadScene("LevelSelection");
-        }
 
-        if (Input.GetKeyDown(KeyCode.Escape) == true)
-        {
-            SceneManager.LoadScene("LevelSelection");
-        }
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)  //Function that runs when player enters a collider (with collider having trigger)
-    {
-        if (collision.name == "LevelSelectionBoard")  //If collider is named LevelSelectionBoard
-        {
-            canUseLevelSelectionBoard = true;  //Can use board
-            selectionBoard.color = Color.green;
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D collision)  //Function that runs when player exits a collider (with collider having trigger)
-    {
-        if (collision.name == "LevelSelectionBoard")
-        {
-            canUseLevelSelectionBoard = false;  //Cant use board
-            selectionBoard.color = Color.red;
-        }
     }
 }
